a5f8ec79f6fe4c3a05a146e085909a84b0670313
tom-effernelli <tom.effernelli@gmail.com>
2025-11-25 14:45:12 +0100

    feat: implermenting encoding/decoding functions

--- GPT.py
+++ GPT.py
@@ -3,3 +3,10 @@ import torch
 with open('dataset.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
