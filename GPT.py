e19f7e441775b7b1e8c636eec5e32d681a1989bd
tom-effernelli <tom.effernelli@gmail.com>
2025-11-25 16:05:45 +0100

    feat: adding get_batch

--- GPT.py
+++ GPT.py
@@ -1,5 +1,7 @@
 import torch
 
+torch.manual_seed(1337)
+
 with open('dataset.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
@@ -13,7 +15,17 @@ decode = lambda l: ''.join([itos[i] for i in l])
 
 data = torch.tensor(encode(text), dtype=torch.long)
 
-split = int(0.9*len(data))
-train_data = data[:split]
-val_data = data[split:]
+k = int(0.9*len(data))
+train_data = data[:k]
+val_data = data[k:]
+
+batch_size = 4
+block_size = 8
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
 
