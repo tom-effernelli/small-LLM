d1b1da00820ca2011dc42e83d04f1febd85eb21e
tom-effernelli <tom.effernelli@gmail.com>
2025-11-27 20:25:40 +0100

    feat: implementing forward

--- GPT.py
+++ GPT.py
@@ -41,5 +41,11 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
     def forward(self, idx, targets):
-        logits = self.token_embedding_table(idx)
-        return logits
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        B, T, C = logits.shape
+        logits = logits.view(B*T, C)
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        
+        return logits, loss
