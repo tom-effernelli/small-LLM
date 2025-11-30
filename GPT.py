def612b6730857e48e15911161895d4f2662fae9
tom-effernelli <tom.effernelli@gmail.com>
2025-11-30 10:30:00 +0100

    feat: testing

--- GPT.py
+++ GPT.py
@@ -40,25 +40,29 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
-    def forward(self, idx, targets):
+    def forward(self, idx, targets=None):
         logits = self.token_embedding_table(idx) # (B,T,C)
 
-        B, T, C = logits.shape
-        logits = logits.view(B*T, C)
-        targets = targets.view(B*T)
-        loss = F.cross_entropy(logits, targets)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
 
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
             logits, loss = self(idx)
-            logits = logits[:, -1, :]
-            probs = F.softmax(logits, dim=1)
-            idx_next = torch.multinomial(probs, num_samples=1)
-            idx = torch.cat((idx, idx_next), dim=1)
+            logits = logits[:, -1, :] # (B, C)
+            probs = F.softmax(logits, dim=1) # (B, C)
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
+# Testing phase
 m = BigramLanguageModel()
 xb, yb = get_batch('train')
 logits, loss = m(xb, yb)
