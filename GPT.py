62ba6af9ff10b8b6b1e533cdf1c0e0f3cbb1338a
tom-effernelli <tom.effernelli@gmail.com>
2025-12-04 19:10:45 +0100

    feat: implementing multiheadattention

--- GPT.py
+++ GPT.py
@@ -76,13 +76,23 @@ class Head(nn.Module):
         wei = F.softmax(wei, dim=-1) # (B, T, T)
         return wei @ v # (B, T, head_size)
 
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -91,7 +101,7 @@ class BigramLanguageModel(nn.Module):
         tok_embd = self.token_embedding_table(idx) # (B,T,C)
         pos_embd = self.position_embedding_table(torch.arange(T, device=idx.device)) # (T, C)
         x = tok_embd + pos_embd # (B, T, C)
-        x = self.sa_head(x)
+        x = self.sa_heads(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
