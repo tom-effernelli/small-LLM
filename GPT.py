fix: scaling up model

GPT.py
@@ -8,14 +8,14 @@ torch.manual_seed(1337)
 mode = 'gen' # 'gen' || 'train'
 
 # Hyperparameters
-batch_size = 32
-block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
 n_head = 6
 n_layer = 6
 dropout = 0.2